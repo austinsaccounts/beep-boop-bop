3f725b8cc7141d9c6f13ed103d7bbf13be6cd402
Austin Cummings <Austins.accounts@gmail.com>
2018-08-17 11:57:59 -0700

    properly traanslate all numbers into their expected parameters and console log the results

--- js/scripts.js
+++ js/scripts.js
@@ -5,41 +5,34 @@ $(document).ready(function() {
     event.preventDefault();
     var usersNumber = $("#numberInput").val();
         usersNumber = parseInt(usersNumber);
+
+
     for (var i = 0; i<=usersNumber; i++) {
       if (i % 3 === 0) {
-        console.log("divisible by 3");
-      }
-      else {
-        console.log(i);
-      }
-
+        console.log("I'm sorry, Dave. I'm afraid I can't do that.");
+      } else if (i.toString().match(0) !== null){
+            console.log("BEEP");
+    } else if (i.toString().match(1) !== null){
+          console.log("BOOP");
+  } else {
+      console.log(i);
     }
-
-
-
-
-
     $("#output").append(numberInput );
-  });
+  };
+});
+})
 
 
 
-    // $("#panic").click(function() {
+
+
+
+
+// $("#panic").click(function() {
     //   console.log("panic fired")
     //     $("#panic").hide();
     //     $(".revealed").show();
     //   });
-
-
-});
-
-
-
-
-
-
-
-
 
 
 
