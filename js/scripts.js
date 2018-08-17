3f9f4537ad6e6046274f7ceb1565765320807aa2
Austin Cummings <Austins.accounts@gmail.com>
2018-08-17 10:33:18 -0700

    create a working active button and be linked to jquery and bootstrap, however cdn for jquery or something seems to have an issue, is being ignored for now

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
 // numberInput is the formName
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $("#panic").click(function() {
+      console.log("button worked")
+        $("#panic").hide();
+        $("")
+      $( "#target" ).submit();
+        $("#panic")
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+// $("#panic").hide();
+// $("#numberInput").show();
+// console.log( "ready!" );
