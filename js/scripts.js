3449c6e6f74d9b88a689cedb94eb3cfc87901288
Austin Cummings <Austins.accounts@gmail.com>
2018-08-23 23:41:59 -0700

    seperate  business logic from user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,27 @@
-function translator(usersNumber) {
-  var responseStatement = "";
-  for (var i = 0; i<=usersNumber; i++) {
-    if (i % 3 === 0) {
-      responseStatement += ("I'm sorry, Dave. I'm afraid I can't do that. <br>");
-    } else if (i.toString().match(0) !== null){
-        responseStatement += ("beep <br>");
-    } else if (i.toString().match(1) !== null){
-        responseStatement += ("boop <br>");
-    } else {
-        responseStatement += (" " + i + " <br>")
-    }
-  };
-  return responseStatement;
+//business logic
+function translator(i) {
+  var responseStatement;
+  if (i % 3 === 0) {
+    responseStatement = "I'm sorry, Dave. I'm afraid I can't do that.";
+  } else if (i.toString().match(0) !== null){
+        responseStatement = "beep";
+  } else if (i.toString().match(1) !== null){
+        responseStatement = "boop";
+  } else {
+    responseStatement = (i);
+  }
+    console.log(responseStatement);
+    return responseStatement;
 };
-
-
+//user interface
 $(document).ready(function() {
   $("#beep-boop-bop").submit(function(event) {
-    $("#output").text("");
     event.preventDefault();
-    var usersNumber = $("#numberInput").val();
-        usersNumber = parseInt(usersNumber);
-        $("#output").append(translator(usersNumber));
-})
-})
-
-
-
-
-
-
-
-
-
-
-
-// $("#panic").hide();
-// $("#numberInput").show();
-// $("#output").append( "ready!" );
+    var usersNumber = parseInt($("#numberInput").val());
+    for (var i = 0 ; i < usersNumber+1 ; i++) {
+      translatedNumber = translator(i).toString();
+      console.log("translatednumber is " + translatedNumber);
+      $("#output").append(" " + translatedNumber + " <br>");
+    }
+  })
+});
